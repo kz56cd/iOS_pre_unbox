88332ed52245332f8dedb76c5b288de9e46f39cd
kz56cd <kz56cd@gmail.com>
2017-02-24 10:46:39 +0900

    Try Transformations

--- preUnbox/User.swift
+++ preUnbox/User.swift
@@ -6,11 +6,13 @@
 //  Copyright © 2017年 msano. All rights reserved.
 //
 
+import UIKit
 import Unbox
 
 struct User {
     let name: String
     let age: Int
+    let firstURL: UniqueURL
     let profession: Profession
     let runningDistance: Int
     let primaryDeviceName: String
@@ -20,6 +22,7 @@ extension User: Unboxable {
     init(unboxer: Unboxer) throws {
         name = try unboxer.unbox(key: "name")
         age = try unboxer.unbox(key: "age")
+        firstURL = try unboxer.unbox(key: "firstURL")                                   // Transformations
         profession = try unboxer.unbox(key: "profession")                               // Built-in enum support
         runningDistance = try unboxer.unbox(keyPath: "activities.running.distance")     // Key path support
         primaryDeviceName = try unboxer.unbox(keyPath: "devices.0")                     // Key path support
@@ -29,4 +32,21 @@ extension User: Unboxable {
 enum Profession: Int, UnboxableEnum {
     case developer
     case astronaut
+}
+
+struct UniqueURL: UnboxableByTransform {
+    typealias UnboxRawValue = String
+    
+    let url: URL
+    
+    init?(urlString: String) {
+        guard let url: URL = URL(string: urlString) else {
+            return nil
+        }
+        self.url = url
+    }
+    
+    static func transform(unboxedValue: String) -> UniqueURL? {
+        return UniqueURL(urlString: unboxedValue)
+    }
 }
